3e5aaba4581aeb9f391ae2fe1b4d97b9bbd47e43
José Pedro Dias <jose.dias@arkadium.com>
2024-04-16 01:08:39 +0100

    wip

--- Scripts/merge_grid.gd
+++ Scripts/merge_grid.gd
@@ -5,7 +5,7 @@ class_name MergeGrid
 @export var columns = 12
 @export var rows    = 12
 
-#var highlighted = Vector2i(2, 1)
+var highlighted = Vector2i(0, 0)
 
 const TILE_SET_ID = 0
 const DEFAULT_LAYER = 0
@@ -29,16 +29,20 @@ func _ready():
 	for y in rows:
 		for x in columns:
 #warning-ignore:integer_division
-			#var coords = Vector2i(x - columns / 2, y - rows / 2)
-			#set_tile_cell(coords, "1")
+			var coords = Vector2i(x - columns / 2, y - rows / 2)
+			set_tile_cell(coords, "1")
 			pass
 			
-	set_highlight(Vector2i(2, 1))
+	set_highlight(Vector2i(0, 0))
 			
 func set_tile_cell(coords: Vector2i, cell_type: String):
 	set_cell(DEFAULT_LAYER, coords, TILE_SET_ID, CELLS[cell_type])
 	
 func set_highlight(coords: Vector2i):
-	#highlighted = coords
+	highlighted = coords
 	clear_layer(HIGHLIGHTS_LAYER)
 	set_cell(HIGHLIGHTS_LAYER, coords, TILE_SET_ID, CELLS["highlight"])
+
+func _input(event: InputEvent):
+	if !(event is InputEventMouseButton) || !event.pressed:
+		print(event)
