1fa718f5bf6b941400e0e79008e1dbabbf0bb251
José Pedro Dias <jose.dias@arkadium.com>
2024-04-19 00:39:11 +0100

    progress

---
+++ README.md
@@ -0,0 +1,7 @@
+# TODO
+
+- display UI overlay
+  - level nr
+  - goal
+- display shrinking bar
+- change level (4 possible)
--- Scripts/merge_grid.gd
+++ Scripts/merge_grid.gd
@@ -6,12 +6,17 @@ class_name MergeGrid
 @export_range(1, 40) var columns = 12
 @export_range(1, 40) var rows    = 12
 
+@export_category("props")
+@export_range(1, 4) var level = 1
+
 @export_category("audios")
 @export var fillAudio: Node
 @export var incomingTickAudio: Node
 @export var mergeAudio: Node
 @export var mistakeAudio: Node
 @export var penaltyAudio: Node
+
+var rng = RandomNumberGenerator.new()
 
 var highlighted = null
 
@@ -29,6 +34,33 @@ const CELLS = {
 	"7": Vector2i(0, 2),
 	"8": Vector2i(1, 2),
 	"highlight": Vector2i(2, 2),
+}
+
+var levels = {
+	1: {
+		"size": [5, 5],
+		"penalty_countdown": 75.0,
+		"fill_countdown": 2.0,
+		"goal_number": 5,
+	},
+	2: {
+		"size": [7, 7],
+		"penalty_countdown": 60.0,
+		"fill_countdown": 2.0,
+		"goal_number": 6,
+	},
+	3: {
+		"size": [8, 8],
+		"penalty_countdown": 45.0,
+		"fill_countdown": 2.0,
+		"goal_number": 7,
+	},
+	4: {
+		"size": [9, 9],
+		"penalty_countdown": 30.0,
+		"fill_countdown": 1.5,
+		"goal_number": 8,
+	},
 }
 
 func _ready():
@@ -49,7 +81,8 @@ func _ready():
 	timer.start()
 
 func _every_sec() -> void:
-	fillAudio.play() # TODO to use when numbers are added back
+	pass
+	#fillAudio.play() # TODO to use when numbers are added back
 	#incomingTickAudio.play() # TODO to use when bar is about to exhaust (seconds 4 to 1)
 	#mergeAudio.play()
 	#mistakeAudio.play()
@@ -61,7 +94,13 @@ func set_tile_cell(coords: Vector2i, num: int):
 	set_cell(DEFAULT_LAYER, coords, TILE_SET_ID, CELLS[num_s])
 	var td = get_cell_tile_data(DEFAULT_LAYER, coords, TILE_SET_ID)
 	td.set_custom_data('num', num)
-	
+
+func get_tile_num(coord: Vector2i):
+	var td = get_cell_tile_data(DEFAULT_LAYER, coord, TILE_SET_ID)
+	if td:
+		return td.get_custom_data('num')
+	return null
+
 func set_highlight(coords: Vector2i, state: bool):
 	#highlighted = coords
 	clear_layer(HIGHLIGHTS_LAYER)
@@ -84,21 +123,45 @@ func _input(event: InputEvent):
 		highlighted = null
 		set_highlight(coord, false)
 	
-		var td = get_cell_tile_data(DEFAULT_LAYER, coord, TILE_SET_ID)
-		var td0 = get_cell_tile_data(DEFAULT_LAYER, coord0, TILE_SET_ID)
-		
-		if td != null && td0 != null:
-			var v  = td.get_custom_data('num')
-			var v0 = td0.get_custom_data('num')
+		var v = get_tile_num(coord)
+		var v0 = get_tile_num(coord0)
 
-			if v == v0:
-				erase_cell(DEFAULT_LAYER, coord0)
-				v += 1
-				set_cell(	DEFAULT_LAYER, coord, TILE_SET_ID, CELLS["%s" % v])
-				var td2 = get_cell_tile_data(DEFAULT_LAYER, coord, TILE_SET_ID)
-				td2.set_custom_data('num', v)
-				mergeAudio.play()
-			else:
-				mistakeAudio.play()
-				# TODO: decrease bar
-			
+		if v == v0:
+			erase_cell(DEFAULT_LAYER, coord0)
+			v += 1
+			set_tile_cell(coord, v)
+			mergeAudio.play()
+		else:
+			mistakeAudio.play()
+			# TODO: decrease bar
+
+func clear_cells():
+	var positions = get_board_positions()
+	for p in positions:
+		if get_tile_num(p) == null:
+			continue
+		var r = rng.randf_range(0.0, 1.0)
+		if r < 0.25:
+			erase_cell(DEFAULT_LAYER, p)
+	
+func fill_empty_cell():
+	var positions = get_board_positions()
+	var p = rng.randi_range(0, positions.size())
+	set_tile_cell(p, 1)
+
+func get_board_positions() -> Array:
+	var arr: Array = []
+	for y in rows:
+		for x in columns:
+			arr.push_back(Vector2i(
+				int(x - columns/2.0),
+				int(y - rows   /2.0)
+			))
+	return arr
+
+func get_matching_positions(arr: Array, fn) -> Array:
+	var arr2: Array = []
+	for p in arr:
+		if fn.call(p):
+			arr2.push_back(p)
+	return arr2
