9110b99ceb7909892372b14af985d286174a9441
José Pedro Dias <jose.dias@arkadium.com>
2024-04-19 19:11:44 +0100

    basic bar

--- Scripts/merge_grid.gd
+++ Scripts/merge_grid.gd
@@ -16,6 +16,10 @@ class_name MergeGrid
 @export var mistakeAudio: Node
 @export var penaltyAudio: Node
 
+@export_category("UI")
+@export var label: Label
+@export var bar: Panel
+
 var rng = RandomNumberGenerator.new()
 
 var highlighted = null
@@ -68,7 +72,7 @@ var lvl = levels[level]
 var bar_left = lvl["penalty_countdown"]
 var fill_left = lvl["fill_countdown"]
 
-func _ready():
+func _ready() -> void:
 	clear_layer(DEFAULT_LAYER)
 	
 	for y in rows:
@@ -79,7 +83,10 @@ func _ready():
 			)
 			set_tile_cell(coords, 1) #??
 	
-func _process(delta):
+	_update_label()
+	_update_bar(1.0)
+	
+func _process(delta: float) -> void:
 	if bar_left - delta <= 0:
 		clear_cells()
 		bar_left = lvl["penalty_countdown"]
@@ -94,7 +101,7 @@ func _process(delta):
 	else:
 		fill_left -= delta
 
-func set_tile_cell(coords: Vector2i, num: int):
+func set_tile_cell(coords: Vector2i, num: int) -> void:
 	var num_s = "%s" % num
 	set_cell(DEFAULT_LAYER, coords, TILE_SET_ID, CELLS[num_s])
 	var td = get_cell_tile_data(DEFAULT_LAYER, coords, TILE_SET_ID)
@@ -106,32 +113,48 @@ func get_tile_num(coord: Vector2i):
 		return td.get_custom_data('num')
 	return null
 
-func set_highlight(coords: Vector2i, state: bool):
+func set_highlight(coords: Vector2i, state: bool) -> void:
 	clear_layer(HIGHLIGHTS_LAYER)
 	if state:
 		set_cell(HIGHLIGHTS_LAYER, coords, TILE_SET_ID, CELLS["highlight"])
 	else:
 		erase_cell(HIGHLIGHTS_LAYER, coords)
 
-func _validate_coords(coords):
+func _validate_coords(coords: Vector2i) -> bool:
 	var min_x = int(-columns/2.0)
 	var min_y = int(-rows/2.0)
 	var x = coords[0]
 	var y = coords[1]
 	return x >= min_x && x < min_x + columns && y >= min_y && y < min_y + rows
 
-func _input(event: InputEvent):
-	if !(event is InputEventMouseButton) || !event.pressed:
-		return
-		
+func _update_label():
+	label.text = "level:%d  goal:%s  %ds" % [1, 2, 3]
+	
+
+# r is how full, where 0 is empty
+func _update_bar(r: float) -> void:
+	var w = 900 * r
+	bar.set_size(Vector2(w, 50))
+	var sb = bar.get_theme_stylebox("panel")
+	sb.set("bg_color", Color(1.0, r, 0.0))
+	bar.position.x = 0.0 - w/2.0
+
+	#bar.material.set_
+	# TODO change color and size
+
+func _input(event: InputEvent) -> void:
+	# escape exits the game
+	if event.is_action_pressed("ui_cancel"): get_tree().quit()
+	
+	# ignore events from types other than input event
+	if !(event is InputEventMouseButton) || !event.pressed: return
+	
+	# convert coords and make sure they're in the board
 	var coord = local_to_map(get_local_mouse_position())
-	
-	print(coord)
 	var is_valid = _validate_coords(coord)
-	if !is_valid:
-		print('early abort')
-		return
+	if !is_valid: return
 	
+	# handle click cell logic
 	if highlighted == null:
 		# first pair click
 		highlighted = coord
@@ -155,12 +178,14 @@ func _input(event: InputEvent):
 			v += 1
 			set_tile_cell(coord, v)
 			mergeAudio.play()
+			_update_label()
+			_update_bar(rng.randf())
 		else:
 			# numbers differ, penalize player!
 			mistakeAudio.play()
 			bar_left -= 1.0
 
-func clear_cells():
+func clear_cells() -> void:
 	var positions = get_board_positions()
 	for p in positions:
 		if get_tile_num(p) == null:
@@ -169,7 +194,7 @@ func clear_cells():
 		if r < 0.25:
 			erase_cell(DEFAULT_LAYER, p)
 	
-func fill_empty_cell():
+func fill_empty_cell() -> void:
 	var positions = get_board_positions()
 	var p = positions[ rng.randi_range(0, positions.size()-1) ]
 	set_tile_cell(p, 1)
